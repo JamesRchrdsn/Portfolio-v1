d38fb032e09b4a0b2b1f90193d0f9a120ec3cf4b
James Rchrdsn <laurent.james63@gmail.com>
2024-06-20 17:07:43 +0200

    mise en place du service mail

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// Modal
+
 function openModal(modalId) {
   document.getElementById(modalId).style.display = "block";
 }
@@ -14,3 +16,48 @@ window.onclick = function (event) {
     }
   }
 };
+
+// Envoi mail
+
+(function () {
+  emailjs.init("service_n48ib6i");
+})();
+
+function sendEmail(event) {
+  event.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const subject =
+    "Contact Portfolio: " + document.getElementById("subject").value;
+  const message = document.getElementById("message").value;
+
+  const templateParams = {
+    name: name,
+    email: email,
+    subject: subject,
+    message: message,
+  };
+
+  emailjs.send("service_n48ib6i", "template_f5drs8f", templateParams).then(
+    function (response) {
+      console.log("SUCCESS!", response.status, response.text);
+    },
+    function (error) {
+      console.log("FAILED...", error);
+    }
+  );
+
+  emailjs.send("service_n48ib6i", "template_wf1glem", templateParams).then(
+    function (response) {
+      console.log("SUCCESS!", response.status, response.text);
+      alert("Votre message a été envoyé avec succès !");
+    },
+    function (error) {
+      console.log("FAILED...", error);
+      alert("L'envoi du message a échoué. Veuillez réessayer plus tard.");
+    }
+  );
+}
+
+document.getElementById("contactForm").addEventListener("submit", sendEmail);
